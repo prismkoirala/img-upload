d831a0df1a560f696603247adee5d9c6f5d3a827
prismkoirala <prizamkoirala@gmail.com>
2024-12-18 14:03:23 -0600

    changes django V

--- pujari_backend/settings.py
+++ pujari_backend/settings.py
@@ -121,6 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = "static/"
 
 MEDIA_URL = '/media/'
